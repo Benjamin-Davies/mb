1f26f2f0989fc7862e7259065b4bcd064832be80
Benjamin Davies <bentendavies@gmail.com>
2021-08-28 10:20:45 +1200

    Re-add help flag

--- src/options.cpp
+++ src/options.cpp
@@ -13,6 +13,7 @@ Options parse_options(int argc, const char *const *argv)
 {
   po::options_description desc("Allowed options");
   desc.add_options()
+      ("help,h", "Print help message")
       ("maildir,d", po::value<fs::path>()->default_value("INBOX"), "Directory containing mail")
       ("uid,u", po::value<int>(), "UID of email to edit/print path of")
       ("toggle,t", po::value<std::string>(), "Toggle the specified flags")
